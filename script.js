67fecd766f5e6a28a3722d11a6b9a21878a95bc9
jtec-dev <aderojuadedayo@gmail.com>
2024-10-20 21:15:38 +0100

    calculator

---
+++ script.js
@@ -0,0 +1,29 @@
+let display = document.getElementById("display");
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    display.value += operator;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function Operator() {
+    display.value = Math.sqrt(parseFloat(display.value))
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
